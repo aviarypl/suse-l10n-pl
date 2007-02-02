67e58c77c596a2b63472ac3aad44a8c59f27155a
Stanisław Małolepszy <smalolepszy@aviary.pl>
2007-02-02 12:00:57 +0000

    Utworzenie brancha SLED10SP1

---
+++ lcn/lcn_updatefiles.sh
@@ -0,0 +1,86 @@
+mv CheckHardware.pl.po checkhardware.pl.po
+mv Linux-PAM.pl.po linux-pam.pl.po
+mv NetworkManager-novellvpn.pl.po networkmanager-novellvpn.pl.po
+mv NetworkManager-openvpn.pl.po networkmanager-openvpn.pl.po
+mv NetworkManager.pl.po networkmanager.pl.po
+mv NetworkManager-vpnc.pl.po networkmanager-vpnc.pl.po
+mv RELEASE-NOTES-sled.pl.po release-notes-sled.pl.po 
+intltool-update --dist --gettext-package=../../50-pot/apparmor-parser apparmor-parser.pl
+intltool-update --dist --gettext-package=../../50-pot/apparmor-utils apparmor-utils.pl
+intltool-update --dist --gettext-package=../../50-pot/banshee banshee.pl
+intltool-update --dist --gettext-package=../../50-pot/beagle beagle.pl
+intltool-update --dist --gettext-package=../../50-pot/bootloader bootloader.pl
+intltool-update --dist --gettext-package=../../50-pot/checkhardware checkhardware.pl
+intltool-update --dist --gettext-package=../../50-pot/compiz compiz.pl
+intltool-update --dist --gettext-package=../../50-pot/cracklib cracklib.pl
+intltool-update --dist --gettext-package=../../50-pot/f-spot f-spot.pl
+intltool-update --dist --gettext-package=../../50-pot/gnome-main-menu gnome-main-menu.pl
+intltool-update --dist --gettext-package=../../50-pot/gnome-patch-translation gnome-patch-translation.pl
+intltool-update --dist --gettext-package=../../50-pot/gnome-power-manager gnome-power-manager.pl
+intltool-update --dist --gettext-package=../../50-pot/hal hal.pl
+intltool-update --dist --gettext-package=../../50-pot/initial initial.pl
+intltool-update --dist --gettext-package=../../50-pot/kernel-update-tool kernel-update-tool.pl
+intltool-update --dist --gettext-package=../../50-pot/kerry kerry.pl
+intltool-update --dist --gettext-package=../../50-pot/kfiledialog kfiledialog.pl
+intltool-update --dist --gettext-package=../../50-pot/kinternet kinternet.pl
+intltool-update --dist --gettext-package=../../50-pot/kio_sysinfo kio_sysinfo.pl
+intltool-update --dist --gettext-package=../../50-pot/kpowersave_additional_strings kpowersave_additional_strings.pl
+intltool-update --dist --gettext-package=../../50-pot/kpowersave kpowersave.pl
+intltool-update --dist --gettext-package=../../50-pot/krpmview krpmview.pl
+intltool-update --dist --gettext-package=../../50-pot/kwin_SUSE2 kwin_SUSE2.pl
+intltool-update --dist --gettext-package=../../50-pot/limal-apparmor-control limal-apparmor-control.pl
+intltool-update --dist --gettext-package=../../50-pot/limal-ca-mgm limal-ca-mgm.pl
+intltool-update --dist --gettext-package=../../50-pot/limal-nfs-server limal-nfs-server.pl
+intltool-update --dist --gettext-package=../../50-pot/limal limal.pl
+intltool-update --dist --gettext-package=../../50-pot/limal-runlevel limal-runlevel.pl
+intltool-update --dist --gettext-package=../../50-pot/linux-pam linux-pam.pl
+intltool-update --dist --gettext-package=../../50-pot/linuxrc linuxrc.pl
+intltool-update --dist --gettext-package=../../50-pot/login login.pl
+intltool-update --dist --gettext-package=../../50-pot/networkmanager-novellvpn networkmanager-novellvpn.pl
+intltool-update --dist --gettext-package=../../50-pot/networkmanager-openvpn networkmanager-openvpn.pl
+intltool-update --dist --gettext-package=../../50-pot/networkmanager networkmanager.pl
+intltool-update --dist --gettext-package=../../50-pot/networkmanager-vpnc networkmanager-vpnc.pl
+intltool-update --dist --gettext-package=../../50-pot/pam_pwcheck pam_pwcheck.pl
+intltool-update --dist --gettext-package=../../50-pot/pam_unix2 pam_unix2.pl
+intltool-update --dist --gettext-package=../../50-pot/patterns-sled patterns-sled.pl
+intltool-update --dist --gettext-package=../../50-pot/patterns-sles patterns-sles.pl
+intltool-update --dist --gettext-package=../../50-pot/power-management power-management.pl
+intltool-update --dist --gettext-package=../../50-pot/profile_chooser profile_chooser.pl
+intltool-update --dist --gettext-package=../../50-pot/release-notes-sled release-notes-sled.pl
+intltool-update --dist --gettext-package=../../50-pot/resapplet resapplet.pl
+intltool-update --dist --gettext-package=../../50-pot/rug rug.pl
+intltool-update --dist --gettext-package=../../50-pot/sax sax.pl
+intltool-update --dist --gettext-package=../../50-pot/scpm_cli scpm_cli.pl
+intltool-update --dist --gettext-package=../../50-pot/scpm_groups scpm_groups.pl
+intltool-update --dist --gettext-package=../../50-pot/scpm_lib scpm_lib.pl
+intltool-update --dist --gettext-package=../../50-pot/slideshow-sled slideshow-sled.pl
+intltool-update --dist --gettext-package=../../50-pot/sumf sumf.pl
+intltool-update --dist --gettext-package=../../50-pot/suseplugger suseplugger.pl
+intltool-update --dist --gettext-package=../../50-pot/susetranslations susetranslations.pl
+intltool-update --dist --gettext-package=../../50-pot/tomboy tomboy.pl
+intltool-update --dist --gettext-package=../../50-pot/update-desktop-files update-desktop-files.pl
+intltool-update --dist --gettext-package=../../50-pot/xgl-settings xgl-settings.pl
+intltool-update --dist --gettext-package=../../50-pot/ximian-artwork ximian-artwork.pl
+intltool-update --dist --gettext-package=../../50-pot/zen-updater zen-updater.pl
+intltool-update --dist --gettext-package=../../50-pot/zmd zmd.pl
+intltool-update --dist --gettext-package=../../50-pot/zypp zypp.pl
+intltool-update --dist --gettext-package=../../50-pot/kcmbeagle kcmbeagle.pl
+intltool-update --dist --gettext-package=../../50-pot/kryptomedia kryptomedia.pl
+intltool-update --dist --gettext-package=../../50-pot/zypper zypper.pl
+intltool-update --dist --gettext-package=../../50-pot/xen-vm-install xen-vm-install.pl
+intltool-update --dist --gettext-package=../../50-pot/control-center2 control-center2.pl
+intltool-update --dist --gettext-package=../../50-pot/eiciel eiciel.pl
+intltool-update --dist --gettext-package=../../50-pot/gnome-screensaver gnome-screensaver.pl
+intltool-update --dist --gettext-package=../../50-pot/libgnomesu libgnomesu.pl
+intltool-update --dist --gettext-package=../../50-pot/libssui libssui.pl
+intltool-update --dist --gettext-package=../../50-pot/vmx-manager vmx-manager.pl
+intltool-update --dist --gettext-package=../../50-html/html-help-boot html-help-boot.pl
+intltool-update --dist --gettext-package=../../50-html/html-help-install html-help-install.pl
+mv checkhardware.pl.po CheckHardware.pl.po 
+mv linux-pam.pl.po Linux-PAM.pl.po 
+mv networkmanager-novellvpn.pl.po NetworkManager-novellvpn.pl.po 
+mv networkmanager-openvpn.pl.po NetworkManager-openvpn.pl.po 
+mv networkmanager.pl.po NetworkManager.pl.po 
+mv networkmanager-vpnc.pl.po NetworkManager-vpnc.pl.po 
+mv release-notes-sled.pl.po RELEASE-NOTES-sled.pl.po
+
